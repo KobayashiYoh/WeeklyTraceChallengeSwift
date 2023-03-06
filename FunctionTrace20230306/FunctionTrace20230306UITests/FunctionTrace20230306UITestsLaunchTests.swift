ace11efdc9cb0f639be2b5b200892cb2d0a37dec
KobayashiYoh <y.kobayashi@4nonome.com>
2023-03-06 15:49:12 +0900

    create project

---
+++ FunctionTrace20230306/FunctionTrace20230306UITests/FunctionTrace20230306UITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  FunctionTrace20230306UITestsLaunchTests.swift
+//  FunctionTrace20230306UITests
+//
+//  Created by 小林陽 on 2023/03/06.
+//
+
+import XCTest
+
+final class FunctionTrace20230306UITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
