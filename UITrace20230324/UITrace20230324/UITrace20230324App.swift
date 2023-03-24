043f46580b2e47c2b3e739384bb6606b8fbaa1b3
KobayashiYoh <y.kobayashi@4nonome.com>
2023-03-24 21:50:09 +0900

    create project

---
+++ UITrace20230324/UITrace20230324/UITrace20230324App.swift
@@ -0,0 +1,17 @@
+//
+//  UITrace20230324App.swift
+//  UITrace20230324
+//
+//  Created by 小林陽 on 2023/03/24.
+//
+
+import SwiftUI
+
+@main
+struct UITrace20230324App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
