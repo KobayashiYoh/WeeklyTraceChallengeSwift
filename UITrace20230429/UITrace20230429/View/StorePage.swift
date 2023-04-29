baf87818a189b7ea7543cf6de12a7a734fbe2076
KobayashiYoh <y.kobayashi@4nonome.com>
2023-04-29 22:25:15 +0900

    StorePageを作成

---
+++ UITrace20230429/UITrace20230429/View/StorePage.swift
@@ -0,0 +1,23 @@
+//
+//  StorePage.swift
+//  UITrace20230429
+//
+//  Created by 小林陽 on 2023/04/29.
+//
+
+import SwiftUI
+
+struct StorePage: View {
+    var body: some View {
+        VStack {
+            Text("Top Charts")
+        }
+        .padding()
+    }
+}
+
+struct StorePage_Previews: PreviewProvider {
+    static var previews: some View {
+        StorePage()
+    }
+}
