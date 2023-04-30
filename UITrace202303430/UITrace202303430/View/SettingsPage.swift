799ff7bb80569cf157b5532a71c859e405ef192e
KobayashiYoh <y.kobayashi@4nonome.com>
2023-04-30 23:49:33 +0900

    ナビゲーションバーを追加

--- UITrace202303430/UITrace202303430/View/SettingsPage.swift
+++ UITrace202303430/UITrace202303430/View/SettingsPage.swift
@@ -8,26 +8,31 @@
 import SwiftUI
 
 struct SettingsPage: View {
+    init() {
+        UINavigationBar.appearance().titleTextAttributes = [.font : UIFont(name: "Pacifico-Regular", size: 17)!]
+    }
     var body: some View {
-        VStack (alignment: .leading) {
-            Spacer().frame(height: 32)
-            VStack {
-                SettingsTitleLabel(title: "アプリ情報")
-                SettingsItem(title: "プライバシーポリシー", trailing: AnyView(Text(">")));
-                Divider()
-                SettingsItem(title: "利用規約", trailing: AnyView(Text(">")));
-                Divider()
-                SettingsItem(title: "アプリバージョン", trailing: AnyView(Text("v1.0.0")));
-                Divider()
-            }
-            Spacer().frame(height: 36)
-            VStack {
-                SettingsTitleLabel(title: "その他");
-                SettingsItem(title: "ログアウトする", trailing: AnyView(Text("")))
-                Divider()
-            }
-            Spacer()
-        }.padding()
+        NavigationView {
+            VStack (alignment: .leading) {
+                Spacer().frame(height: 32)
+                VStack {
+                    SettingsTitleLabel(title: "アプリ情報")
+                    SettingsItem(title: "プライバシーポリシー", trailing: AnyView(Text(">")));
+                    Divider()
+                    SettingsItem(title: "利用規約", trailing: AnyView(Text(">")));
+                    Divider()
+                    SettingsItem(title: "アプリバージョン", trailing: AnyView(Text("v1.0.0")));
+                    Divider()
+                }
+                Spacer().frame(height: 36)
+                VStack {
+                    SettingsTitleLabel(title: "その他");
+                    SettingsItem(title: "ログアウトする", trailing: AnyView(Text("")))
+                    Divider()
+                }
+                Spacer()
+            }.padding().navigationBarTitle("Settings", displayMode: .inline)
+        }
     }
 }
 
