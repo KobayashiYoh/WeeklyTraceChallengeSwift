6404f14f37a93c7f3216bd0fbb13dcafc14e1d60
KobayashiYoh <y.kobayashi@4nonome.com>
2023-05-02 22:01:34 +0900

    create project

---
+++ UITrace20230502/UITrace20230502/UITrace20230502App.swift
@@ -0,0 +1,17 @@
+//
+//  UITrace20230502App.swift
+//  UITrace20230502
+//
+//  Created by 小林陽 on 2023/05/02.
+//
+
+import SwiftUI
+
+@main
+struct UITrace20230502App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ UITrace20230502/UITrace20230502/View/SectionItem.swift
@@ -0,0 +1,20 @@
+//
+//  SectionItem.swift
+//  UITrace20230502
+//
+//  Created by 小林陽 on 2023/05/02.
+//
+
+import SwiftUI
+
+struct SectionItem: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SectionItem_Previews: PreviewProvider {
+    static var previews: some View {
+        SectionItem()
+    }
+}
