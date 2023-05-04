7d9973ccb006196d63965cefc6dd7ea0b94cd010
KobayashiYoh <y.kobayashi@4nonome.com>
2023-05-04 23:56:12 +0900

    SegementedPickerを追加

--- UITrace20230504/UITrace20230504/ContentView.swift
+++ UITrace20230504/UITrace20230504/ContentView.swift
@@ -8,9 +8,27 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var selection = 0
+    
+    init() {
+        let appearance = UISegmentedControl.appearance()
+        let foregroundColor = UIColor.systemBlue
+        appearance.selectedSegmentTintColor = foregroundColor
+        appearance.setTitleTextAttributes([
+            .foregroundColor: foregroundColor
+        ], for: .normal)
+        appearance.setTitleTextAttributes([
+            .foregroundColor: UIColor.white
+        ], for: .selected)
+    }
+
     var body: some View {
         NavigationView {
             VStack {
+                Picker(selection: $selection, label: Text("ラベル")) {
+                    Text("Paid Apps").tag(0)
+                    Text("Free Apps").tag(1)
+                }.pickerStyle(.segmented).padding(.horizontal, 16)
                 StoreItem(imageName: "store-item", rank: 1, title: "title", subTitle: "subTitle", price: 0, isInstalled: false)
                 StoreItem(imageName: "store-item", rank: 2, title: "title", subTitle: "subTitle", price: 0, isInstalled: true)
                 StoreItem(imageName: "store-item", rank: 3, title: "title", subTitle: "subTitle", price: 6.99, isInstalled: false)
