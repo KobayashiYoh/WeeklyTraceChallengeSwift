c6b092d6689ae17fa8b940957ec16c26d9baf37c
KobayashiYoh <y.kobayashi@4nonome.com>
2023-07-13 22:14:42 +0900

    背景画像を追加

--- UITrace20230713/UITrace20230713/ContentView.swift
+++ UITrace20230713/UITrace20230713/ContentView.swift
@@ -10,7 +10,7 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         ZStack {
-            Color.green.ignoresSafeArea()
+            Color(red: 0, green: 0, blue: 0, opacity: 0.2).background(Image("background").resizable()).ignoresSafeArea()
             VStack {
                 Spacer().frame(height: 200)
                 Text("Qiita Feed App").foregroundColor(.white).font(.custom("Pacifico-Regular", size: 36))
@@ -18,8 +18,8 @@ struct ContentView: View {
                 Text("-PlayGround-").foregroundColor(.white).bold()
                 Spacer()
                 Button(action: {}){
-                    Text("ログイン").foregroundColor(.white).bold()
-                }
+                    Text("ログイン").foregroundColor(.white).bold().frame(maxWidth: .infinity, maxHeight: 50)
+                }.background(Color(red: 70/255, green: 131/255, blue: 0/255)).cornerRadius(24).padding()
                 Spacer().frame(height: 16)
                 Button(action: {}){
                     Text("ログインせずに続ける").foregroundColor(.white).bold()
